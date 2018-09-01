d9ea93a16d8395e199111085c3a238fab996b870
Wiktor Idzikowski <wiktor.idzikowski@gmail.com>
2018-09-01 13:52:47 +0200

    [#3.1] Add view for displaying note details

--- App.js
+++ App.js
@@ -1,57 +1,104 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Text, TextInput } from 'react-native';
+import { View, StyleSheet, ScrollView, Text, TextInput, Button } from 'react-native';
 import { Constants } from 'expo';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends Component {
-  state = {
-    actions: [],
-    value: '',
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
   };
 
+  state = {
+    items: [
+      { id: 1, name: 'Foo', title:'Note title 1' },
+      { id: 2, name: 'Bar', title: 'Note title 2' },
+      { id: 3, name: 'Foo3',  title:'Note title 3'}
+    ],
+    value: '',
+  };
 
   render() {
     return (
       <View style={styles.container}>
-      <View>
+        <View>
+          <Text>Home</Text>
+          <TextInput
+              placeholder="Enter text"
+              returnKeyType="done"
+              value={this.state.value}
+              onChangeText={this.textChanged}
+              onSubmitEditing={this.submit}
+          />
+        </View>
 
-      </View>
-      <TextInput
-                placeholder="Enter text"
-                returnKeyType="done"
-                value={this.state.value}
-                onChangeText={this.textChanged}
-                onSubmitEditing={this.submit}
-              />
         <ScrollView style={styles.scrollView}>
-        <Text style={styles.header}>List of my notes</Text>
-        {this.state.actions.map(({ content }) => (
-          <View key={content} style={styles.item}>
-            <Text style={styles.content}>{content}</Text>
-          </View>
-        ))}
+          <Text style={styles.header}>List of my notes</Text>
+          {this.state.items.map(item => (
+            <View key={item.id} style={styles.item}>
+              <Text style={styles.content}>{item.name}</Text>
+              <Button
+                key={item.id}
+                title={`Show item ${item.id}`}
+                onPress={() => this.props.navigation.navigate('Item', item)}
+              />
+            </View>
+          ))}
         </ScrollView>
       </View>
     );
   }
+
   textChanged = text =>
     this.setState(state => ({
       value: text,
     }));
 
-  submit = text =>
+  submit = () =>
   this.setState(state => ({
-    actions: state.actions.concat({
-      content: state.value,
+    items: state.items.concat({
+      id: state.items.length + 1,
+      name: state.value,
+      title: state.value,
     }),
     value: '',
   }));
 }
 
+
+const ItemScreen = ({ navigation }) => (
+  <View>
+    <Text>Item #{navigation.state.params.id}</Text>
+    <Text>Item #{navigation.getParam('id', '-')}</Text>
+    <Text>{navigation.getParam('name', 'No name')}</Text>
+    <Text>{navigation.getParam('title', 'No title')}</Text>
+    <Button title="Go back" onPress={() => navigation.goBack()} />
+  </View>
+);
+ItemScreen.navigationOptions = ({ navigation }) => ({title: `Item #${navigation.getParam('id')}`})
+
+const Navigator = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Item: {
+      screen: ItemScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends Component {
+  render() {
+    return <Navigator />;
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: 'aliceblue',
   },
