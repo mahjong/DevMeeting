bc0e96d76f81efab5d3f50973c0bb6cf27a3ec6a
Wiktor Idzikowski <wiktor.idzikowski@gmail.com>
2018-09-01 10:38:54 +0200

    [#1.2] Display list of notes

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView, Text } from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends Component {
+  state = {
+    items: new Array(100).fill(0).map((a, i) => i).map(i => ({
+      title: `Note title ${i}`,
+      content: `Note content  #${i}. This is my ${i} note.`,
+    })),
+  };
+
   render() {
     return (
       <View style={styles.container}>
-      <Text style={styles.title}>First Note</Text>
-      <Text style={styles.content}>This is my first note!</Text>
+        <ScrollView style={styles.scrollView}>
+          {this.state.items.map(({ title, content }) => (
+            <View key={title} style={styles.item}>
+              <Text style={styles.title}>{title}</Text>
+              <Text style={styles.content}>{content}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     );
   }
@@ -20,6 +33,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
+  },
+  item: {
+    marginBottom: 20,
   },
   title: {
     fontWeight: 'bold',
